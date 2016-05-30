786768efb48eee1f59351cb5a798ee2230c50853
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-05-30 16:04:17 +0530

    Added Google

---
+++ app/google/google.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: "ng-social-google",
+	templateUrl: "app/templates/google.component.html"
+})
+
+export class GoogleSocialComponent{
+	private _user : Object;
+	
+}
