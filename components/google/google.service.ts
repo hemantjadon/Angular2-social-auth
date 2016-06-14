5c2dd8f658d6ebc7f844c40004c55a2db85d5a39
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-06-14 19:27:22 +0530

    Some changes : probably won't work

--- components/google/google.service.ts
+++ components/google/google.service.ts
@@ -8,14 +8,12 @@ export class GoogleService {
 	constructor( private http : Http ) 
 	{ }
 
-	public login_request( params : URLSearchParams ) : Promise<Object>{
-		let url = `https://accounts.google.com/o/oauth2/v2/auth`
-		return new Promise<Object>(( resolve : Function , reject : Function )=>{
-			this.http.get(url,{ search : params })
-					 .toPromise()
-					 .then((response) => {
-						 console.log(response);
-					 });
+	public login_request( params : URLSearchParams ){
+		let url = `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`;
+		window.addEventListener('load', function (event) {
+			var code = event.timeStamp;
+			console.log(code);
 		});
+		window.open(url,"target");
 	}
 }
